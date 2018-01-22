4729e4d1169fc99994b0163bd733eaf41dd5ce0a
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2018-01-22 14:59:30 +0000

    Updated DataDownload file

--- DataPreProcessing/DataDownload.sh
+++ DataPreProcessing/DataDownload.sh
@@ -7,8 +7,16 @@ get_abs_filename() {
   echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
+# Position rest of commands into DataPreProcessing directory
+cd "$(dirname "$0")"
+echo $PWD
+
 # Target download directory for data
 targetDataDir=$(get_abs_filename "Data/")
+
+if ! [ -d "$targetDataDir" ]; then
+    mkdir $targetDataDir
+fi
 
 # Download ENCODE DNase tracks to the target "Data/" directory
 wget -r ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeAwgDnaseUniform -P $targetDataDir
