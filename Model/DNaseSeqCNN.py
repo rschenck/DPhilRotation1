11748edf413a36970e53bcd5d90e2334c039454b
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2018-02-02 10:28:24 +0000

    Updated some hyperparameter option choices

--- Model/DNaseSeqCNN.py
+++ Model/DNaseSeqCNN.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
-
 import os
 import sys
-from optparse import OptionParser
-import logging
 
-import keras as ks
-import numpy as np
-import h5py
+try:
+    from optparse import OptionParser
+    import logging
+
+    import keras as ks
+    import numpy as np
+    import h5py
+except Exception as e:
+    print(e, file=sys.stdout)
+    sys.exit("ERROR: Unable to load dependencies.")
 
 log = logging.getLogger(__name__)
 out_hdlr = logging.StreamHandler(sys.stdout)
@@ -16,9 +20,20 @@ out_hdlr.setLevel(logging.INFO)
 log.addHandler(out_hdlr)
 log.setLevel(logging.INFO)
 
-# log.error("NOPE")
-# log.debug("FUCK YES")
-# log.info("OH My")
+# CHOOSE: log.error("") log.debug("")   log.info("")
+
+# Parse command line options
+def OptionParsing():
+    usage = 'usage: %prog [options] -f <*.h5>'
+    parser = OptionParser(usage)
+    parser.add_option('-f', '--h5File', dest='ModelData', default=None, help="*.h5 file created using CreateHDF5.py containing the train, test, and validation data sets.")
+    parser.add_option('--opt', '--optimizer', dest='usrOpt', default='rmsprop', help="Optimizer used for training, either 'adam' or 'rmsprop'. Default='rmsprop'.")
+    parser.add_option('-m', '--momentum', dest='Momentum', default=0.98, help="Momentum value range(0,1) for optimization momentum. Default=0.98")
+    parser.add_option('-l', '--learnrate', dest='LearningRate', default=0.002, help="Learning rate range(0,1) for optimization learning rate. Default=0.002.")
+    (options, args) = parser.parse_args()
+    if not options.ModelData:
+        parser.error('ERROR: Must provide a *.h5 file with train, test, and validation data.')
+    return(options, parser)
 
 class Data:
     '''
@@ -62,27 +77,29 @@ class ModelArch:
     '''
     Model Architecture Information
     '''
-    def __init__(self):
+    def __init__(self, Options):
         self.ConvLayers = [300,200,200]
         self.ConvFilterSizes = [19,11,7]
         self.ConvPoolWidth = [3,4,4]
         #self.HiddenUnit = [1000,1000]
         #self.HiddenDropouts = [0.3,0.3]
-        #self.LearningRate = 0.002 # Optimizer Options
+        self.LearningRate = 0.002 # Optimizer Options
         #self.WeightNorm = 7 # Used to cap weight params within an epoch, not sure here...google...
-        #self.Momentum = 0.98 # Optimizer Options
+        self.Momentum = 0.98 # Optimizer Options
+        self.usrOpt = 'rmsprop'
+        self.CheckOptions(Options)
 
-# Parse command line options
-def OptionParsing():
-    usage = 'usage: %prog [options] -f <*.h5>'
-    parser = OptionParser(usage)
-    parser.add_option('-f', '--h5File', dest='ModelData', default=None, help="*.h5 file created using CreateHDF5.py containing the train, test, and validation data sets.")
-    (options, args) = parser.parse_args()
-    if not options.ModelData:
-        parser.error('ERROR: Must provide a *.h5 file with train, test, and validation data.')
-    return(options, parser)
+    def CheckOptions(self, Options):
+        if Options.usrOptim not in ['adam','rmsprop']:
+            log.error("Unknown optimization chosen, please enter 'adam' or 'rmsprop'")
+            sys.exit()
+        else:
+            self.usrOpt = Options.usrOpt
+            self.LearningRate = Options.LearningRate
+            self.Momentum = Options.Momentum
 
-def TrainModel(Options, data, arch):
+
+def ConstructModelArch(Options, data, arch):
 
     # Initialize a sequential model architecture
     model = ks.Sequential()
@@ -95,6 +112,12 @@ def TrainModel(Options, data, arch):
             model.add(ks.layers.MaxPool1D(pool_size=arch.ConvPoolWidth[i]))
 
     # Compile the model
+    # TODO implement options for optimizer using learning rate and momentum
+    # Optimizer options
+    if arch.usrOpt == 'adam':
+        pass
+    elif arch.usrOpt == 'rmsprop':
+        pass
     model.compile(optimizer='adam',
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
@@ -106,6 +129,6 @@ if __name__=="__main__":
 
     # Get Data and ModelArch (Model Architecture Class)
     data = Data(Options)
-    arch = ModelArch()
+    archOptions = ModelArch()
 
-    TrainModel(Options, data, arch)
+    ConstructModelArch(Options, data, archOptions)
