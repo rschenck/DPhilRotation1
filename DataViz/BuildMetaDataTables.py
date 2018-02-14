09c958bb1ccd67b45791ebc49d52b1a8948c4624
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2018-02-14 14:42:50 +0000

    Updated checkpointer so it doesn't do it

--- DataViz/BuildMetaDataTables.py
+++ DataViz/BuildMetaDataTables.py
@@ -3,6 +3,8 @@ import sys
 import os
 from collections import OrderedDict
 
+import pandas as pd
+
 def pullBedFile(FilePath):
     bedFile = "%s/DataPreProcessing/Data/data_beds.txt" % (FilePath)
     with open(bedFile, 'r') as inFile:
@@ -33,29 +35,41 @@ def pullEncode(FilePath, bedfile):
                 id = line[1]
             details.update({line[0]:line[1]})
 
-        if len([id for item in bedfile if id == item])==1:
-            metadata.update({id:details})
-        elif len([id for item in bedfile if id == item])==0:
-            pass
+        metadata.update({id:details})
 
     return(metadata)
 
-def CheckEncode(EncodeMD, bedfiles):
+def pullEncodeFiles(FilePath):
+    ENCODE_files = "%s/DataPreProcessing/Data/ENCODE/files.txt" % (FilePath)
+    with open(ENCODE_files, 'r') as inFile:
+        lines = OrderedDict({line.rstrip('\n').split('\t',1)[0]:{line2.split('=')[0]:line2.split('=')[1] for line2 in line.rstrip('\n').split('\t',1)[1].split('; ')} for line in inFile.readlines()})
+    return(lines)
+
+def CheckEncode(EncodeMD, bedfiles, encode_files):
     for item in bedfiles:
         if 'ENCODE' in bedfiles[item]:
             try:
                 EncodeMD[item]
             except KeyError:
-                print(item)
+                try:
+                    itemGetAlt = encode_files[bedfiles[item].split('/')[len(bedfiles[item].split('/'))-1]]['cell']
+                    # print(itemGetAlt)
+                    EncodeMD[itemGetAlt]
+                except KeyError:
+                    print(item)
+                    sys.exit("Unable to get cell info")
+
+def pullRoadMap(FilePath):
+    roadmap_Data = "%s/DataPreProcessing/Data/RoadmapGenomics/jul2013.roadmapData.qc.xlsx"%(FilePath)
+    df = pd.read_excel(roadmap_Data, sheet_name='Consolidated_EpigenomeIDs_summa')
 
 def main():
     FilePath = os.path.dirname(os.path.abspath(__file__)).rstrip('./DataViz/')
-    # ENCODE_files = "%s/DataPreProcessing/Data/ENCODE/files.txt"%(FilePath)
+    encode_files = pullEncodeFiles(FilePath)
     bedFile = pullBedFile(FilePath)
     ENCODE_metadata = pullEncode(FilePath, bedFile)
-    roadmap_Data = "%s/DataPreProcessing/Data/RoadmapGenomics/jul2013.roadmapData.qc.xlsx"%(FilePath)
-
-    CheckEncode(ENCODE_metadata, bedFile)
+    roadmap_metadata = pullRoadMap(FilePath)
+    CheckEncode(ENCODE_metadata, bedFile, encode_files)
 
 
 if __name__=="__main__":
