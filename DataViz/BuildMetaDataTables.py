9c4823d829944bbeb7cf414a4f0421ffb6d04fea
Ryan O. Schenck <ryanschenck@mail.usf.edu>
2018-02-14 15:57:17 +0000

    Built cell data table

--- DataViz/BuildMetaDataTables.py
+++ DataViz/BuildMetaDataTables.py
@@ -14,7 +14,7 @@ def pullBedFile(FilePath):
 def pullEncode(FilePath, bedfile):
     ENCODE_metadata = "%s/DataPreProcessing/Data/ENCODE/cv.ra"%(FilePath)
     with open(ENCODE_metadata, 'r') as inFile:
-        lines = [line for line in inFile.readlines() if line.startswith('#')==False]
+        lines = [line for line in inFile.readlines()]
 
     chunks = []
     chunksout = []
@@ -22,6 +22,8 @@ def pullEncode(FilePath, bedfile):
         if line == "\n":
             chunks.append(chunksout)
             chunksout=[]
+        elif line.startswith('#'):
+            pass
         else:
             chunksout.append(line)
     del chunks[0]
@@ -45,6 +47,11 @@ def pullEncodeFiles(FilePath):
         lines = OrderedDict({line.rstrip('\n').split('\t',1)[0]:{line2.split('=')[0]:line2.split('=')[1] for line2 in line.rstrip('\n').split('\t',1)[1].split('; ')} for line in inFile.readlines()})
     return(lines)
 
+def pullRoadMap(FilePath):
+    roadmap_Data = "%s/DataPreProcessing/Data/RoadmapGenomics/jul2013.roadmapData.qc.xlsx"%(FilePath)
+    df = pd.read_excel(roadmap_Data, sheet_name='Consolidated_EpigenomeIDs_summa')
+    return(df)
+
 def CheckEncode(EncodeMD, bedfiles, encode_files):
     for item in bedfiles:
         if 'ENCODE' in bedfiles[item]:
@@ -59,9 +66,83 @@ def CheckEncode(EncodeMD, bedfiles, encode_files):
                     print(item)
                     sys.exit("Unable to get cell info")
 
-def pullRoadMap(FilePath):
-    roadmap_Data = "%s/DataPreProcessing/Data/RoadmapGenomics/jul2013.roadmapData.qc.xlsx"%(FilePath)
-    df = pd.read_excel(roadmap_Data, sheet_name='Consolidated_EpigenomeIDs_summa')
+def CheckRoadmap(roadmap, bedfile):
+    roadmapRows = {}
+    for item in bedfile:
+        if 'RoadmapGenomics' in bedfile[item]:
+            for i in range(roadmap.shape[0]):
+                eid = roadmap.iloc[i, 5]
+                if item==eid:
+                    roadmapRows.update({item:i})
+
+    return(roadmapRows)
+
+def BuildTable(bedfiles, encode_files, ENCODE_metadata, roadmap_metadata, roadmapRows, FilePath):
+
+    linesToWrite=[]
+    for item in bedfiles:
+        # Cell  AltID   Karyotype   Type    Tissue    Tier    Treatment   Lineage Description
+        outLine = [item]
+        if 'ENCODE' in bedfiles[item]:
+            try:
+                data = ENCODE_metadata[item]
+                outLine.append('NA')
+            except KeyError:
+                try:
+                    itemGetAlt = encode_files[bedfiles[item].split('/')[len(bedfiles[item].split('/'))-1]]['cell']
+                    outLine.append(itemGetAlt)
+                    data = ENCODE_metadata[itemGetAlt]
+                except KeyError:
+                    print(item)
+                    sys.exit("Unable to get cell info")
+
+            try:
+                outLine.append(data['karyotype'])
+            except KeyError:
+                outLine.append("Unspecified")
+
+            outLine.append(data['type'].replace(" ","").lower())
+            try:
+                outLine.append(data['tissue'])
+            except KeyError:
+                outLine.append("Unspecified")
+
+            outLine.append(data['tier'])
+            try:
+                treat = encode_files[bedfiles[item].split('/')[len(bedfiles[item].split('/'))-1]]['treatment']
+            except KeyError:
+                if bedfiles[item].split('/')[len(bedfiles[item].split('/'))-1] == "wgEncodeAwgDnaseDukeHuh75UniPk.narrowPeak.gz":
+                    treat= encode_files["wgEncodeAwgDnaseDukeHuh7.5UniPk.narrowPeak.gz"]['treatment']
+            outLine.append(treat)
+            try:
+                outLine.append(data['lineage'])
+            except KeyError:
+                outLine.append('Unspecified')
+
+            outLine.append(data['description'])
+
+        elif 'RoadmapGenomics' in bedfiles[item]:
+            r = roadmapRows[item]
+            row = roadmap_metadata.iloc[r,]
+
+            outLine.append(row['Epigenome ID (EID)'])
+            outLine.append('normal')
+
+            outLine.append(row['TYPE'].lower())
+            outLine.append(row['ANATOMY'].lower())
+            outLine.append('Unspecified')
+            outLine.append('None')
+            outLine.append('Unspecified')
+            outLine.append(row['Standardized Epigenome name'])
+
+        else:
+            sys.exit("Screwed")
+
+        linesToWrite.append('\t'.join(outLine))
+
+    with open(FilePath + "/DataViz/Rotation1App/Data/CellLineInfo.txt", 'w') as outFile:
+        outFile.write('\t'.join(['Cell','AltID','Karyotype','Type','Tissue','Tier','Treatment','Lineage','Description'])+'\n')
+        outFile.write('\n'.join(linesToWrite))
 
 def main():
     FilePath = os.path.dirname(os.path.abspath(__file__)).rstrip('./DataViz/')
@@ -70,6 +151,9 @@ def main():
     ENCODE_metadata = pullEncode(FilePath, bedFile)
     roadmap_metadata = pullRoadMap(FilePath)
     CheckEncode(ENCODE_metadata, bedFile, encode_files)
+    roadmapRows = CheckRoadmap(roadmap_metadata, bedFile)
+
+    BuildTable(bedFile, encode_files, ENCODE_metadata, roadmap_metadata, roadmapRows, FilePath)
 
 
 if __name__=="__main__":
